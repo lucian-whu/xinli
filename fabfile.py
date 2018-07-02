0aaecd9380ef76a46cef363855a876ef4a6dd94c
Xin Li <lucian2015@qq.com>
2018-07-02 23:01:08 +0800

    20180701开始部署

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,12 @@
 from fabric.api import *
 
-env.hosts = raw_input('enter your host address: ')
-env.user = raw_input('enter  your username: ')
-env.password = raw_input('enter your password: ')
+# env.hosts = raw_input('enter your host address: ')
+# env.user = raw_input('enter  your username: ')
+# env.password = raw_input('enter your password: ')
+
+env.hosts = ['119.23.27.37']
+env.user = 'root'
+env.password = 'Huaketj@151020'
 def hello():
     print('hello world!')
 
